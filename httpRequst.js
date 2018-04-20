3cc9682746a2217616ae57ef9719ee713bf29ef1
王振兴 <wangzhenxingvip@gmail.com>
2018-04-20 19:37:24 +0800

    CHG:http封装

--- httpRequst.js
+++ httpRequst.js
@@ -1,6 +1,18 @@
 'use strict';
 
 const http = require('http');
+const querystring = require('querystring');
+
+let HOST = 'xxjs.dtdjzx.gov.cn';
+
+let commonHeaders = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'Origin': 'http://xxjs.dtdjzx.gov.cn',
+    // 'Referer': 'http://xxjs.dtdjzx.gov.cn/kaishijingsai.html'  //请求来源
+    // Cookie: X-SESSION=e1f1733f-103f-4013-96e2-7f17ad8b026b
+    //X-Requested-With: XMLHttpRequest
+};
 
 function httpRequest(options, data, callback) {
     console.log(`${options.method}: ${options.hostname}${options.path}`);
@@ -29,45 +41,26 @@ function httpRequest(options, data, callback) {
     req.end();
 }
 
-function httpPost(host, path, header, data, callback) {
-    httpRequest(host, path, 'POST', data, header, callback);
+function httpGet({ host = HOST, port = 80, headers = commonHeaders, path, queryParams, callback }) {
+    const options = {
+        hostname: host,
+        port: port,
+        path: `${path}?${querystring.stringify(queryParams)}`,
+        method: 'GET',
+        headers: headers
+    };
+    httpRequest(options, '', callback);
 }
 
-/**
- * http.get(url, (res) => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
-
-        let error;
-        if (statusCode !== 200) {
-            error = new Error('请求失败。\n' +
-                `状态码: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error('无效的 content-type.\n' +
-                `期望 application/json 但获取的是 ${contentType}`);
-        }
-        if (error) {
-            console.error(error.message);
-            // 消耗响应数据以释放内存
-            res.resume();
-            return;
-        }
-
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-                const parsedData = JSON.parse(rawData);
-                // console.log(parsedData);
-                callback(parsedData);
-            } catch (e) {
-                console.error(e.message);
-            }
-        });
-    }).on('error', (e) => {
-        console.error(`错误: ${e.message}`);
-    });
- */
+function httpPost({ host = HOST, port = 80, headers = commonHeaders, path, body, callback }) {
+    const options = {
+        hostname: host,
+        port: port,
+        path: path,
+        method: 'POST',
+        headers: headers
+    };
+    httpRequest(options, body, callback);
+}
 
 module.exports = httpRequest;
